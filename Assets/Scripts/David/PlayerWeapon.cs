b88e5335fa5e38a2ef10a9d842a54596555721d9
David <synlike@gmail.com>
2021-01-06 11:25:12 +0100

    SpecialWeapon

--- Assets/Scripts/David/PlayerWeapon.cs
+++ Assets/Scripts/David/PlayerWeapon.cs
@@ -6,6 +6,8 @@ public class PlayerWeapon : MonoBehaviour
 {
     [SerializeField]
     GameObject projectile;
+    [SerializeField]
+    GameObject projectileSpecial;
 
     [SerializeField]
     Transform shootOrigin;
@@ -14,14 +16,12 @@ public class PlayerWeapon : MonoBehaviour
     private float speed = 5f;
 
     [SerializeField]
-    private float shootRate = 2; // 2 per seconds
+    private int shootsBeforeSpecial = 5;
+    [HideInInspector]
+    public bool canShoot = true;
 
     private int shootCount = 0;
-    [SerializeField]
-    private int shootBeforeSpecial = 5;
-    private bool canShoot = true;
-    private float shootTimer = 0f;
-    
+
 
     void Update()
     {
@@ -29,29 +29,27 @@ public class PlayerWeapon : MonoBehaviour
         {
             if(canShoot)
             {
-                if(shootCount <= 5)
+                if(shootCount <= shootsBeforeSpecial)
                 {
                     GameObject projectileInstance = Instantiate(projectile, shootOrigin.position, Quaternion.identity);
+                    projectileInstance.GetComponent<ProjectileMovement>().AssignPlayerWeapon(this);
                     shootCount++;
                 }
                 else
                 {
                     // Tir Spécial
-                    Debug.Log("Tir Spécial");
+                    GameObject projectileInstance = Instantiate(projectileSpecial, shootOrigin.position, Quaternion.identity);
+                    projectileInstance.GetComponent<ProjectileMovement>().AssignPlayerWeapon(this);
+                    
                     shootCount = 0;
                 }
                 canShoot = false;
             }
         }
+    }
 
-        if(!canShoot)
-        {
-            shootTimer += Time.deltaTime;
-            if (shootTimer >= (1 / shootRate))
-            {
-                canShoot = true;
-                shootTimer = 0f;
-            }
-        }
+    public void ResetShootCount()
+    {
+        shootCount = 0;
     }
 }
--- Assets/Scripts/David/ProjectileMovement.cs
+++ Assets/Scripts/David/ProjectileMovement.cs
@@ -6,7 +6,20 @@ public class ProjectileMovement : MonoBehaviour
 {
     [SerializeField]
     private float speed = 5f;
-    
+
+    private PlayerWeapon playerWeapon;
+
+    private int hitToDestroy = 1;
+    private int hitCount = 0;
+
+    private void Start()
+    {
+        if(gameObject.CompareTag("Special"))
+        {
+            hitToDestroy = 2;
+        }
+    }
+
     void Update()
     {
         transform.position += transform.up * Time.deltaTime * speed;
@@ -15,17 +28,32 @@ public class ProjectileMovement : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log(other.gameObject.tag);
+        hitCount++;
 
         if(other.CompareTag("Ennemy"))
         {
-            Debug.Log("Walah");
             other.gameObject.GetComponent<Ennemy_Script>().DestroyEnnemy();
+            if(hitCount == hitToDestroy)
+            {
+                DestroySelf();
+            }
         }
 
         if (other.CompareTag("Sky"))
         {
-            Debug.Log("Wesh");
+            playerWeapon.ResetShootCount();
+            DestroySelf();
         }
+    }
+
+    private void DestroySelf()
+    {
+        playerWeapon.canShoot = true;
         Destroy(gameObject);
+    }
+
+    public void AssignPlayerWeapon(PlayerWeapon pw)
+    {
+        playerWeapon = pw;
     }
 }
