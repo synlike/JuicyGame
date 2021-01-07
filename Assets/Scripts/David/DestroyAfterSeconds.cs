8d2543cfe9d86ab7418bde53718a2bcdca1c4018
David <synlike@gmail.com>
2021-01-07 18:03:38 +0100

    Explosion destroy after seconds

---
+++ Assets/Scripts/David/DestroyAfterSeconds.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterSeconds : MonoBehaviour
+{
+    [SerializeField]
+    private float time = 2f;
+
+    void Start()
+    {
+        Destroy(gameObject, time);
+    }
+    
+    void Update()
+    {
+        
+    }
+}
