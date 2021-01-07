981365a8d0877fa917937af0cdd9d4fc73a9b32f
David <synlike@gmail.com>
2021-01-07 16:36:08 +0100

    Debug tilt

---
+++ Assets/Scripts/David/ShipTilt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipTilt : MonoBehaviour
+{
+    [SerializeField]
+    private float tilAngle = 30f;
+    [SerializeField]
+    private float rotationSpeed = 5f;
+
+    void Update()
+    {
+        EulerRotate();
+    }
+
+    void EulerRotate()
+    {
+        var input = Input.GetAxis("Horizontal");
+        var currAngles = transform.eulerAngles;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(currAngles.x, tilAngle * -input + 180, currAngles.z), Time.deltaTime * rotationSpeed);
+    }
+}
