1f3891a6e10e2361556d42017f8740397b003c31
David <synlike@gmail.com>
2021-01-07 19:43:27 +0100

    Assign Tilt and Orb

--- Assets/Scripts/David/EffectActivator.cs
+++ Assets/Scripts/David/EffectActivator.cs
@@ -1,22 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class EffectActivator : MonoBehaviour
 {
     public ScreenShake screenShake;
     public ParticleSystem playerShipTrail;
     public PlayerWeapon playerWeaponScript;
+    public ShipTilt shipTilt;
+    public VisualEffect orb;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Y))
+        if (Input.GetKeyDown(KeyCode.Keypad1))
         {
             Debug.Log("hello");
             screenShake.activateScreenshake = !screenShake.activateScreenshake;
         }
 
-        if (Input.GetKeyDown(KeyCode.U))
+        if (Input.GetKeyDown(KeyCode.Keypad2))
         {
             if (playerShipTrail.isPlaying)
             {
@@ -30,10 +33,30 @@ public class EffectActivator : MonoBehaviour
             }
         }
 
-        if (Input.GetKeyDown(KeyCode.I))
+        if (Input.GetKeyDown(KeyCode.Keypad3))
         {
             playerWeaponScript.activateRecoil = !playerWeaponScript.activateRecoil;
             Debug.Log("Ship Recoil = " + playerWeaponScript.activateRecoil);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad4))
+        {
+            shipTilt.tiltActivated = !shipTilt.tiltActivated;
+            Debug.Log("Ship Tilt = " + shipTilt.tiltActivated);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Keypad5))
+        {
+            if(orb.isActiveAndEnabled)
+            {
+                orb.enabled = false;
+                Debug.Log("Orb Activated");
+            }
+            else
+            {
+                orb.enabled = true;
+                Debug.Log("Orb Deactivated");
+            }
         }
     }
 }
