b06945f6f833e55b8be37c5605f5b980f797313c
David <synlike@gmail.com>
2021-01-07 16:16:48 +0100

    Tilt ship + light ships

--- Assets/Scripts/David/PlayerMovement.cs
+++ Assets/Scripts/David/PlayerMovement.cs
@@ -7,20 +7,36 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField]
     private float speed = 4;
 
+    float tilAngle = 30f;
+    float rotationSpeed = 5f;
+
     Rigidbody rb;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
     }
-    
+
+    void Update()
+    {
+        EulerRotate();
+    }
+
     void FixedUpdate()
     {
         float input = Input.GetAxis("Horizontal");
 
-        if(input != 0)
+        if (input != 0)
         {
             transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, 0, 0);
         }
+    }
+
+    void EulerRotate()
+    {
+        var input = Input.GetAxis("Horizontal");
+        var currAngles = transform.eulerAngles;
+
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(currAngles.x, tilAngle * -input, currAngles.z), Time.deltaTime * rotationSpeed);
     }
 }
